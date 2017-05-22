4387e2f9ca33ce7b377b4ab1c07beef16d6406d6
Methodicalmind <Spiderman11>
2017-05-22 10:47:27 -0600

    upload collections and albums both high and web res

--- web/dbconn.php
+++ web/dbconn.php
@@ -1,8 +1,8 @@
 <?php
     $db = pg_connect("  host=localhost
                         dbname=photosite
-                        user=postgres
-                        password=Spiderman11
+                        user=ps
+                        password=Lasagna1
                     ");
     if (!$db) {
       echo "An error occurred connecting to db.\n";
