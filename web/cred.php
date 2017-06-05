1476ff18d52cacc97e00408e6a26dc10267f71af
Christopher Murray <brothermurray@gmail.com>
2017-06-05 13:39:46 -0600

    new creds

---
+++ web/cred.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+    $user = 'veeusasszvpxeo';
+    $pass = '6f59098d3f322b26311fa50e024ddcd6acbb9f6fc9962f50d77e44a086d52950';
+    $dbName = 'd28mt1hpjpm58r';
+    $dbHost = 'ec2-54-227-237-223.compute-1.amazonaws.com';
+    $dbPort = '5432';
+?>
