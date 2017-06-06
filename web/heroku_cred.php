6a9b09d2458bdc914373d33b4c91ce4af1bfa93b
Methodicalmind <Spiderman11>
2017-06-06 11:28:37 -0600

    heroku should work now

--- web/heroku_cred.php
+++ web/heroku_cred.php
@@ -1,6 +1,6 @@
 <?php
     $user = "veeusasszvpxeo";
-    $password = "6f59098d3f322b26311fa50e024ddcd6acbb9f6fc9962f50d77e44a086d52950";
+    $pass = "6f59098d3f322b26311fa50e024ddcd6acbb9f6fc9962f50d77e44a086d52950";
     $dbHost = "ec2-54-227-237-223.compute-1.amazonaws.com";
     $dbName = "d28mt1hpjpm58r";
     $dbPort = "5432";
